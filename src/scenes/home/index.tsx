cae79097784671fcfa7356137e109a81172471ec
oladejotoyyibolalekan <oladejotoyyibolalekan@gmail.com>
2023-04-05 11:36:22 +0100

    third commit with framer motion

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -3,10 +3,12 @@ import React from 'react'
 import useMediaQuery from '@/hooks/useMediaQuery'
 import  ActionButton from "@/shared/ActionButton";
 import HomePageText from "@/assets/HomePageText.png";
-import HomepageGraphics from "@/assets/HomepageGraphics.png";
-import SponserRedBull from "@/assets/SponserRedBull.png";
-import SponserForbes from "@/assets/SponserForbes.png";
-import SponserFortune from "@/assets/SponserFortune.png";
+import HomePageGraphic from "@/assets/HomePageGraphic.png";
+import SponsorRedBull from "@/assets/SponsorRedBull.png";
+import SponsorForbes from "@/assets/SponsorForbes.png";
+import SponsorFortune from "@/assets/SponsorFortune.png";
+import AnchorLink from 'react-anchor-link-smooth-scroll';
+import { motion } from "framer-motion";
 
 type Props = {
     setSelectedPage: (value: SelectedPage) => void;
@@ -14,27 +16,68 @@ type Props = {
 
 const Home
  = ({setSelectedPage}: Props) => {
-    const isAboveMediumScreens =  useMediaQuerySelect("min-width:1060px");
+    const isAboveMediumScreens =  useMediaQuery("min-width:1060px");
   return (
-    <section id='home' className='gap-16 pbg-gray-20 py-10 md:h-full md:pb-0'>
+    <section id='home' className='gap-16 bg-gray-20 py-10 md:h-full md:pb-0'>
         {/* IMAGE AND MAIN HEADER */}
-        <div>
+        <div className='md:flex mx-auto w-5/6 items-center justify-center md:h-5/6'>
           {/* MAIN HEADER */}
-          <div>
+          <div className='z-10 mt-32 md:basis-3/5'>
             {/* HEADINGS */}
-            <div>
-              <div>
-                <div>
+            <motion.div 
+            className='md:-mt-20'
+            initial="hidden"
+            whileHover={{ scale: 1.2 }}
+            whileInView="visible"
+            viewport={{ once:true, amount: 0.5 }}
+            transition={{ duration: 0.5}}
+            variants={{ 
+              hidden: { opacity: 0, x: -50 },
+              visible: { opacity: 1, x: 0 }
+            }}
+            >
+              <div className='relative'>
+                <div className='before:absolute before:-top-20 before:-left-20 before:z-[-1] md:before:content-evolvetext'>
                   <img src={HomePageText} alt="home-page-text" />
                 </div>
               </div>
+
+              <p className='mt-8 text-sm '>
+                Unrivaled Gym. Unparalleled Training Fitness Classes. World Class Studio to get the Body Shapes That you Dream of.. Get your Dream Body Now.
+              </p>
+            </motion.div>
+
+            {/* ACTIONS */}
+            <div className='mt-8 gap-8 md:justify-start   flex items-center'>
+              <ActionButton setSelectedPage={setSelectedPage}>
+                  Join Now
+              </ActionButton> 
+              <AnchorLink className="text-sm font-bold text-primary-500 underline hover:text-secondary-500"
+              onClick={() => setSelectedPage(SelectedPage.ContactUs)}
+              href={`#${SelectedPage.ContactUs}`}
+              >
+                <p>Learn More</p>
+              </AnchorLink>
             </div>
           </div>
           {/* IMAGE */}
-          <div>
-
+          <div className='flex basis-3/5 justify-center md:z-10 md:ml-40 md:mt-16 md:justify-items-end'>
+            <img src={HomePageGraphic} alt="home-pageGraphic" />
           </div>
         </div>
+
+        {/* SPONSORS */}
+        {isAboveMediumScreens && (
+            <div className='h-[150px] w-full bg-primary-100 py-10'>
+              <div className='mx-auto w-5/6'>
+                <div className='flex w-3/5 items-center justify-between gap-5'>
+                  <img src={SponsorRedBull} alt="redbull-sponsor" />
+                  <img src={SponsorForbes} alt="forbes-sponsor" />
+                  <img src={SponsorFortune} alt="fortune-sponsor" />
+                </div>
+              </div>
+            </div>
+          )}
     </section>
   )
 }

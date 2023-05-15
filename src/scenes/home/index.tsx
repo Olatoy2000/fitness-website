c4d1f50512aae2a1ea852a9d266a3a0d520b0944
oladejotoyyibolalekan <oladejotoyyibolalekan@gmail.com>
2023-05-15 20:04:01 +0100

    first commit

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -20,22 +20,23 @@ const Home
   return (
     <section id='home' className='gap-16 bg-gray-20 py-10 md:h-full md:pb-0'>
         {/* IMAGE AND MAIN HEADER */}
-        <div className='md:flex mx-auto w-5/6 items-center justify-center md:h-5/6'>
+        <motion.div 
+        onViewportEnter={()=> setSelectedPage(SelectedPage.Home)}
+        className='md:flex mx-auto w-5/6 items-center justify-center md:h-5/6'>
           {/* MAIN HEADER */}
           <div className='z-10 mt-32 md:basis-3/5'>
             {/* HEADINGS */}
             <motion.div 
             className='md:-mt-20'
             initial="hidden"
-            whileHover={{ scale: 1.2 }}
+            // whileHover={{ scale: 1.2 }}
             whileInView="visible"
             viewport={{ once:true, amount: 0.5 }}
             transition={{ duration: 0.5}}
             variants={{ 
               hidden: { opacity: 0, x: -50 },
               visible: { opacity: 1, x: 0 }
-            }}
-            >
+            }}>
               <div className='relative'>
                 <div className='before:absolute before:-top-20 before:-left-20 before:z-[-1] md:before:content-evolvetext'>
                   <img src={HomePageText} alt="home-page-text" />
@@ -48,7 +49,17 @@ const Home
             </motion.div>
 
             {/* ACTIONS */}
-            <div className='mt-8 gap-8 md:justify-start   flex items-center'>
+            <motion.div 
+              initial="hidden"
+              whileInView="visible"
+              // whileHover={{ scale: 1.2 }}
+              viewport={{ once: true, amount: 0.5 }}
+              transition={{delay:0.2, duration: 0.5}}
+              variants={{
+                hidden: {opacity: 0, x: -50},
+                visible: {opacity: 1, x: 0}
+              }}
+            className='mt-8 gap-8 md:justify-start flex items-center'>
               <ActionButton setSelectedPage={setSelectedPage}>
                   Join Now
               </ActionButton> 
@@ -58,13 +69,13 @@ const Home
               >
                 <p>Learn More</p>
               </AnchorLink>
-            </div>
+            </motion.div>
           </div>
           {/* IMAGE */}
           <div className='flex basis-3/5 justify-center md:z-10 md:ml-40 md:mt-16 md:justify-items-end'>
             <img src={HomePageGraphic} alt="home-pageGraphic" />
           </div>
-        </div>
+        </motion.div>
 
         {/* SPONSORS */}
         {isAboveMediumScreens && (
